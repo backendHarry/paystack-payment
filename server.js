3e58659f31ccefb17517cb06467c1b9bb9d7d52f
Austin Harrison <osagiedeharrison@yahoo.com>
2021-12-18 08:24:20 +0100

    update build path

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use((err, req, res, next) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "client", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
